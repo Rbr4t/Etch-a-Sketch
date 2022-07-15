a8f64a9f50d73b623891ba6628672886cdc6ed5b
Robert <robertkoor0@gmail.com>
2022-07-15 16:57:02 +0300

    Fixed bug where divs didn't show up in the DOM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hi!")
+
+for (let i=0; i<16; i++){
+    const mainDiv = document.querySelector('.grid');
+    let newDiv = document.createElement('div');
+    newDiv.style.cssText = 'background-color: gray; border: 1px solid black;'
+    //const text = document.createTextNode('test');
+    //newDiv.appendChild(text);
+    mainDiv.appendChild(newDiv);
+}
