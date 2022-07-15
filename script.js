8be8135798201c642f00660d8b73c04c8075b8b2
Robert <robertkoor0@gmail.com>
2022-07-15 19:06:19 +0300

    finished grid

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const mainDiv = document.querySelector('.grid');
-let size = 6
 
-for (let i=0; i<size; i++){
+function createGrid(size) {
+    const mainDiv = document.querySelector('.grid');
+    let cellSize = 500/size;
+    let rowSize = size*cellSize;
+    console.log(rowSize);
     
-    for (let j= 0;j<size; j++){
-        let newDiv = document.createElement('div');
-        newDiv.style.cssText = 'height: 10px; width: 10px;';
     
-        mainDiv.appendChild(newDiv);
+    for (let i=0; i<size; i++){
+        
+        for (let j= 0;j<size; j++){
+            let newDiv = document.createElement('div');
+            newDiv.style.cssText = `height: ${cellSize}px; width: ${cellSize}px;`;
+            mainDiv.appendChild(newDiv);
+        };
+
     };
     
+    mainDiv.style.cssText = `width: ${rowSize}px; height: ${rowSize}px`; //doesn't seem to set the width and height
     
 };
-mainDiv.style.cssText = `width: ${size*10}; height: ${size*10};`;
+
+createGrid(100);
+
