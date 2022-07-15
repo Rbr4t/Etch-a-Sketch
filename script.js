427c7ca74da8e09cc8b7d84d3b6c662e38bc52ba
Robert <robertkoor0@gmail.com>
2022-07-15 17:18:46 +0300

    Added grid generating which almost works.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+const mainDiv = document.querySelector('.grid');
+let size = 6
 
-for (let i=0; i<16; i++){
-    const mainDiv = document.querySelector('.grid');
-    let newDiv = document.createElement('div');
-    newDiv.style.cssText = 'background-color: gray; border: 1px solid black;'
-    //const text = document.createTextNode('test');
-    //newDiv.appendChild(text);
-    mainDiv.appendChild(newDiv);
-}
+for (let i=0; i<size; i++){
+    
+    for (let j= 0;j<size; j++){
+        let newDiv = document.createElement('div');
+        newDiv.style.cssText = 'height: 10px; width: 10px;';
+    
+        mainDiv.appendChild(newDiv);
+    };
+    
+    
+};
+mainDiv.style.cssText = `width: ${size*10}; height: ${size*10};`;
