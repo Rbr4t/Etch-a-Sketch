ba4f9787cb4af1653149b343d8edc0724f9bd809
Robert <robertkoor0@gmail.com>
2022-07-15 21:42:44 +0300

    Did some code cleanup. Trail after hovering.

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 
+//function what creates the grid
 function createGrid(size) {
     const mainDiv = document.querySelector('.grid');
     let cellSize = 500/size;
     let rowSize = size*cellSize;
-    console.log(rowSize);
-    
     
     for (let i=0; i<size; i++){
-        
         for (let j= 0;j<size; j++){
             let newDiv = document.createElement('div');
             newDiv.style.cssText = `height: ${cellSize}px; width: ${cellSize}px;`;
@@ -15,10 +13,20 @@ function createGrid(size) {
         };
 
     };
-    
-    mainDiv.style.cssText = `width: ${rowSize}px; height: ${rowSize}px`; //doesn't seem to set the width and height
-    
 };
 
-createGrid(100);
+createGrid(9);
 
+
+// function responsible for coloring the currently active div
+function colorDiv(event){
+    event.target.classList.add('colored');
+};
+
+let div = document.querySelector('.grid');
+
+//this should be multiple event listener, but it seems to be rather MOUSEDOWN || MOUSEOVER 
+// not MOUSEDOWN && MOUSEOVER
+['mousedown', 'mouseover'].forEach(function(e){
+    div.addEventListener(e, colorDiv, false)
+});
