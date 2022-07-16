14fe29958b257110c151c8d402bc65da8206ebee
Robert <robertkoor0@gmail.com>
2022-07-16 12:57:25 +0300

    Added function which resets the grid and draws new

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
+const mainDiv = document.querySelector('.grid');
 //function what creates the grid
 function createGrid(size) {
-    const mainDiv = document.querySelector('.grid');
+    
     let cellSize = 940/size;
     let rowSize = size*cellSize;
     
@@ -14,13 +14,14 @@ function createGrid(size) {
 
     };
 };
-let gridSize = 32;
+let gridSize = 16;
 createGrid(gridSize);
 
 
 
 // function responsible for coloring the currently active div
 function colorDiv(event){
+    //console.log(event.which)
     if(event.which == 1){
         event.preventDefault()
         event.target.classList.add('colored');
@@ -29,3 +30,26 @@ function colorDiv(event){
 
 const div = document.querySelector('.grid');
 div.addEventListener('mouseover', colorDiv, true)
+
+
+
+//function for deleting the existing grid and drawing new one with new size
+function drawNewGrid() {
+    let newSize = parseInt(prompt("Enter new size: "), 10);
+    
+    while (isNaN(newSize) || newSize<0 || newSize>100) {
+        newSize = prompt("Enter new size: "); 
+    };
+
+    let first = mainDiv.firstElementChild;
+    while (first) {
+        first.remove();
+        first = mainDiv.firstElementChild;
+    };
+    createGrid(newSize);
+
+}
+
+//New grid
+const newGrid = document.querySelector('.change-grid');
+newGrid.addEventListener('click', drawNewGrid)
